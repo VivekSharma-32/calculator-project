6ea4dbdc2a43b9116bdba03c010b6c2b4b8f8c3c
VivekSharma-32 <vivek.sharma1275@gmail.com>
2023-01-30 22:16:06 +0530

    changes

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-let screen = document.querySelector(".screen");
-let buttons = document.querySelectorAll(".btn");
-let clear = document.querySelector(".btn-clear");
-let equal = document.querySelector(".btn-equal");
+let result = document.getElementById("inputText");
 
-buttons.forEach(function (button) {
-  button.addEventListener("click", function (e) {
-    let value = e.target.dataset.num;
-    screen.value += value;
-  });
-});
+let calculate = (number) => {
+  result.value += number;
+};
 
-equal.addEventListener("click", function (e) {
-  if (screen.value == "") {
-    screen.value = "";
-  } else {
-    let answer = eval(screen.value);
-    screen.value = answer;
+let Result = () => {
+  try {
+    result.value = eval(result.value);
+  } catch (err) {
+    alert("Enter the valid input value");
   }
-});
-clear.addEventListener("click", function (e) {
-  screen.value = "";
-});
+};
+
+function clr() {
+  result.value = "";
+}
+
+let del = () => {
+  result.value = result.value.slice(0, -1);
+};
